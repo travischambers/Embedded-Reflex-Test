93b99731ff89148701ed277e210e788eb6431ce2
Travis Chambers <tjchambers32@gmail.com>
2015-05-30 14:02:22 -0600

    updated travis.yml

---
+++ src/Main.c
@@ -0,0 +1,18 @@
+#include main.h
+#include "Executor.h"
+
+int AppMain(void)
+{
+  Executor_Init();
+
+  while(Executor_Run());
+
+  return 0;
+}
+
+#ifndef TEST
+int main(void)
+{
+  return AppMain();
+}
+#endif // TEST
