0411ca13f62a298a2a5423ca0cc8f9399270b759
Travis Chambers <tjchambers32@gmail.com>
2015-09-07 12:50:08 -0600

    Update unity_test_summary.rb

--- reflex-test/vendor/ceedling/vendor/unity/auto/unity_test_summary.rb
+++ reflex-test/vendor/ceedling/vendor/unity/auto/unity_test_summary.rb
@@ -4,9 +4,6 @@
 #   [Released under MIT License. Please refer to license.txt for details]
 # ==========================================
 
-require 'coveralls'
-Coveralls.wear!
-
 #!/usr/bin/ruby
 #
 # unity_test_summary.rb
